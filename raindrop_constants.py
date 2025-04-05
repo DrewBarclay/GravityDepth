ff0cf921ca6370b88b47bb05dbe9ea67125d0c50
Drew Barclay <drew@entrupy.com>
2025-04-05 00:46:12 -0400

    fixed constants

--- raindrop_constants.py
+++ raindrop_constants.py
@@ -2,7 +2,7 @@ from pygame.math import Vector2
 
 # RainDrop constants
 DEFAULT_VELOCITY_Y = 200.0
-GRAVITY_ACCELERATION = Vector2(0, 8000.0)
+GRAVITY_ACCELERATION = Vector2(0, 128000.0)
 MAX_VELOCITY_MAGNITUDE = 400.0
 MAX_UPWARD_VELOCITY = 200.0
 DEFAULT_WIDTH = 1
@@ -14,6 +14,6 @@ REPULSION_FORCE = 100
 # Damping factor applied to velocity during collision
 RAIN_COLLISION_FRICTION = 10
 # Multiplier for increasing repulsion based on depth inside object
-DEPTH_REPULSION_MULTIPLIER = 2.0
+DEPTH_REPULSION_MULTIPLIER = 10.0
 # Friction coefficient for air
-RAIN_AIR_FRICTION = 0.01
+RAIN_AIR_FRICTION = 1.5
